91e73edf53f381d98cb768efced74625dfcf2c09
nfadili <nfadili@uw.edu>
2015-12-23 22:02:38 -0800

    First background algorithm is working!

--- src/script.js
+++ src/script.js
@@ -8,32 +8,18 @@ generateBackground(ctx, myImageData);
 //broken! doesnt go all the way down the canvas. plot() isnt determined by anything
 //it jsut acts in a predictable way.
 function generateBackground(ctx, myImageData) {
-    var corna = 0;
-    var cornb = 0;
-    var side = 11;
+    var thingy = 0;
     var data = myImageData.data;
-    for (var i = 0; i < data.length; i+=myImageData.width*4) {
-        for (var j = i; j < myImageData.width*4; j+=4) {
-            // var x = (corna + i) * side % myImageData.height;
-            // var y = (cornb + j) * side % myImageData.width;
-            // var c = Math.round(Math.pow(x, 2) + Math.pow(y, 2));
-
-            var c = corna + i % data.length;
-            if (c % 2 === 0) {
-                plot(data, i, j);
-            }
-            corna++;
-            cornb++;
-        }
+    for (var i = 0; i < data.length; i += 4) {
+        plot(data, i, thingy);
+        thingy += 101;
     }
-    myImageData.data = data;
     ctx.putImageData(myImageData, 0, 0);
 }
 
-function plot(data, x, y) {
-    //console.log(y);
-    data[y] = 255;
-    data[y + 1] = 0;
-    data[y + 2] = 255;
-    data[y + 3] = 255;
+function plot(data, x, thingy) {
+    data[x] = x * thingy % 255;
+    data[x + 1] = 0;
+    data[x + 2] = 0;
+    data[x + 3] = 255;
 }
